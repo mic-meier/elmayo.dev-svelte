db031aeab68afaad927a1a965ff51be78cf93c78
ElMayo <mic.l.meier@gmail.com>
2022-05-04 19:05:02 +0200

    Basic scaffolding and styling

---
+++ src/routes/contact/index.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<h1>Cotact Form</h1>
+<p>TODO...</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<div class= "max-w-prose mx-auto pt-4 pb-12 px-4">
+  <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
