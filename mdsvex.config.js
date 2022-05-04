40fcf407d23e506b8db677dc9abd24afdf164780
ElMayo <mic.l.meier@gmail.com>
2022-05-04 20:36:31 +0200

    feat: Add MDSvex support and styling

---
+++ mdsvex.config.js
@@ -0,0 +1,17 @@
+import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+
+const config = defineConfig({
+	extensions: ['.svelte.md', '.md', '.svx'],
+
+	smartypants: {
+		dashes: 'oldschool'
+	},
+
+	remarkPlugins: [],
+	rehypePlugins: [],
+	layout: {
+		blog: './src/routes/blog/layout.svelte'
+	}
+});
+
+export default config;
